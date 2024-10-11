8e2bd8180a95801736b2cd6c0c4485147631b6c8
Himanshu12866 <manshu010m@gmail.com>
2024-10-11 15:39:20 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-green-500'>Tsting Tailwind</div>
+  )
+}
+
+export default App
