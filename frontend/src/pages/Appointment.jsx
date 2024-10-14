d5fc8390c37dc36c7d2316fedf9dc99ef10c8f72
Himanshu12866 <manshu010m@gmail.com>
2024-10-14 15:09:09 +0530

    "Updated Appointment component with new layout and added doctor information display"

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,31 +1,43 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
+import { assets } from '../assets/assets'
 
 const Appointment = () => {
 
   const { docId } = useParams()
   const { doctors } = useContext(AppContext)
-  console.log(doctors)
   const [docInfo, setDocinfo] = useState(null)
   const fetchInfo = async () => {
-    const docInfo = doctors.find(doc => doc._id === docId)
-    setDocinfo(docInfo)
-    console.log(docInfo)
+    const docInf = doctors.find(doc => doc._id === docId)
+    setDocinfo(docInf)
+
   }
   useEffect(() => {
     fetchInfo()
-    console.log(docInfo)
+
   }, [doctors, docId])
+
   return (
     <div>{
-      <div>
+      <div className='flex flex-col sm:flex-row'>
         <div>
-        <img src={docInfo.image}/>
-
+          <img src={docInfo.image} />
         </div>
-
+        <div>
+          {/* Doctor Informations */}
+          <p>{docInfo.name} <img src={assets.verified_icon} /></p>
+        </div>
+        <div>
+          <p>{docInfo.degree} - {docInfo.speciality}</p>
+          <button>{docInfo.experience}</button>
+        </div>
+        <div>
+          <p>About <img src={assets.info_icon} /></p>
+          <p>{docInfo.about}</p>
+        </div>
       </div>
     }
 
