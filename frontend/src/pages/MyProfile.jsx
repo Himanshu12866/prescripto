a807ab41f019420bd16d859d06df1f917a086eac
Himanshu12866 <manshu010m@gmail.com>
2024-10-15 18:21:32 +0530

    "Added state and conditional rendering to MyProfile component for editing user data"

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,11 +1,45 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from '../assets/assets'
 
 const MyProfile = () => {
+    const [userData, setUserData] = useState({
+        name: 'Himanshu',
+        email: 'himanshu@gmail.com',
+        phone: '1234567890',
+        image: assets.profile_pic,
+        address: {
+            line1: "Satna",
+            line2: "Hyd"
+        },
+        gender: "Male",
+        dob: "2002-10-10"
+    })
+    const [isEdit, setIsEdit] = useState(false)
     return (
         <div>
-           
+            <img src={userData.image} />
+            {
+                isEdit ? <input type='text' value={userData.name} onChange={(e) => setUserData(prev => ({ ...prev, name: e.target.value }))} /> : <p>{userData.name}</p>
+            }
+            <hr />
+            <div>
+                <p>CONTACT INFORMATION</p>
+                <p>Email :</p>
+                <p>{userData.email}</p>
+                <p>Phone :</p>
+                {
+                    isEdit ? <input type='text' value={userData.phone} onChange={(e) => setUserData(prev => ({ ...prev, phone: e.target.value }))} /> : <p>{userData.phone}</p>
+                }
+                <p>Address</p>
+                {
+                    isEdit ? <p>
+                        <input type='text' value={userData.address.line1} onChange={(e) => setUserData(prev => ({ ...prev, address: { ...prev.address, line1: e.target.value } }))} />
+                        <input type='text' value={userData.address.line2} onChange={(e) => setUserData(prev => ({ ...prev, address: { ...prev.address, line2: e.target.value } }))} />
+                    </p> :
+                        <p>{userData.address.line1} <br /> {userData.address.line2}</p>
+                }
+            </div>
         </div>
     )
 }
