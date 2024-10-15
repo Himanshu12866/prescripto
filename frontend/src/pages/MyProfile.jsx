d255967162df74e03d5c0198c91a5e47b2aa1028
Himanshu12866 <manshu010m@gmail.com>
2024-10-15 19:03:01 +0530

    "Updated MyProfile component with various styling and formatting changes, including added classes, uppercase conversions, and input field modifications."

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -17,49 +17,50 @@ const MyProfile = () => {
     })
     const [isEdit, setIsEdit] = useState(false)
     return (
-        <div className='max-w-lg flex flex-col gap-2 text-sm'>
+        <div className='max-w-lg flex flex-col gap-2 text-sm mb-52'>
             <img alt='kadksnd' className='w-36 rounded' src={userData.image} />
             {
-                isEdit ? <input className='bg-zinc-200 rounded font-medium text-2xl mt-2 p-2 text-black' type='text' value={userData.name} onChange={(e) => setUserData(prev => ({ ...prev, name: e.target.value }))} /> : <p className='text-gray-800 font-medium text-2xl mt-2 p-2'>{userData.name}</p>
+                isEdit ? <input className='bg-zinc-200 rounded font-medium text-2xl mt-2 p-2 text-black' type='text' value={userData.name} onChange={(e) => setUserData(prev => ({ ...prev, name: e.target.value }))} /> : <p className='text-gray-800 font-medium text-2xl mt-2 p-2'>{userData.name.toUpperCase()}</p>
             }
             <hr className='bg-zinc-600 h-0.5' />
             <p className='p-1 underline underline-offset-1 text-gray-600 font-medium text-md'>CONTACT INFORMATION</p>
             <div className='grid grid-cols-[1fr_3fr] mt-1 gap-y-2.5'>
-                <p>Email :</p>
-                <p>{userData.email}</p>
-                <p>Phone :</p>
+                <p className='font-medium'>Email :</p>
+                <p className='text-blue-600'>{userData.email.toUpperCase()}</p>
+                <p className='font-medium'>Phone :</p>
                 {
-                    isEdit ? <input type='text' value={userData.phone} onChange={(e) => setUserData(prev => ({ ...prev, phone: e.target.value }))} /> : <p>{userData.phone}</p>
+                    isEdit ? <input type='text' className='bg-zinc-200 p-1 text-black' value={userData.phone} onChange={(e) => setUserData(prev => ({ ...prev, phone: e.target.value }))} /> : <p className='text-blue-600'>{userData.phone}</p>
                 }
-                <p>Address</p>
+                <p className='font-medium'>Address : </p>
                 {
                     isEdit ? <p>
-                        <input type='text' value={userData.address.line1} onChange={(e) => setUserData(prev => ({ ...prev, address: { ...prev.address, line1: e.target.value } }))} />
-                        <input type='text' value={userData.address.line2} onChange={(e) => setUserData(prev => ({ ...prev, address: { ...prev.address, line2: e.target.value } }))} />
+                        <input className='bg-zinc-200 p-1 text-black w-full' type='text' value={userData.address.line1} onChange={(e) => setUserData(prev => ({ ...prev, address: { ...prev.address, line1: e.target.value } }))} />
+                        <br />
+                        <input className='bg-zinc-200 p-1 mt-1 w-full text-black' type='text' value={userData.address.line2} onChange={(e) => setUserData(prev => ({ ...prev, address: { ...prev.address, line2: e.target.value } }))} />
                     </p> :
-                        <p>{userData.address.line1} <br /> {userData.address.line2}</p>
+                        <p className='text-blue-600'>{userData.address.line1.toUpperCase()} <br /> {userData.address.line2.toUpperCase()}</p>
                 }
             </div>
             <p className='p-1 underline underline-offset-1 text-gray-600 font-medium text-md'>BASIC INFORMATION</p>
             <div className='grid grid-cols-[1fr_3fr] mt-1 gap-y-2.5'>
-                <p>Gender</p>
+                <p className='font-medium'>Gender</p>
                 {
-                    isEdit ? <select type='text' value={userData.gender} onChange={(e) =>
+                    isEdit ? <select className='bg-zinc-200 p-1 text-black' type='text' value={userData.gender} onChange={(e) =>
                         setUserData(prev => ({ ...prev, gender: e.target.value }))}>
                         <option value="Not Selected">Not Selected</option>
                         <option value="Male">Male</option>
                         <option value="Female">Male</option>
                     </select> :
-                        <p>{userData.gender}</p>
+                        <p className='text-blue-600'>{userData.gender.toUpperCase()}</p>
                 }
-                <p>DOB :</p> {
-                    isEdit ? <input type='date' value={userData.dob} onChange={(e) => setUserData(prev => ({ ...prev, dob: e.target.value }))} /> : <p>{userData.dob}</p>
+                <p className='font-medium'>DOB :</p> {
+                    isEdit ? <input className='bg-zinc-200 p-1 text-black' type='date' value={userData.dob} onChange={(e) => setUserData(prev => ({ ...prev, dob: e.target.value }))} /> : <p className='text-blue-600'>{userData.dob}</p>
                 }
                 {
-                    isEdit ? <div>
-                        <button className='' onClick={() => setIsEdit(false)}>Save Detials</button> <button onClick={() => setIsEdit(false)}>Cancel</button>
+                    isEdit ? <div className='flex flex-row w-52'>
+                        <button className='rounded bg-primary text-center text-white p-2 m-1' onClick={() => setIsEdit(false)}>Save Detials</button> <button className='rounded bg-primary text-center text-white p-2 m-1' onClick={() => setIsEdit(false)}>Cancel</button>
                     </div>
-                        : <button onClick={() => setIsEdit(true)}>Edit Detials</button>
+                        : <button className='rounded bg-primary text-center text-white p-2 m-1' onClick={() => setIsEdit(true)}>Edit Detials</button>
                 }
             </div>
         </div>
