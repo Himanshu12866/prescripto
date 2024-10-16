f10e83c498c5f049e2a3ed7cdef26f115be205f4
Himanshu12866 <manshu010m@gmail.com>
2024-10-16 17:28:34 +0530

    "Added Cloudinary config to .env and server.js, updated MongoDB URI"

---
+++ backend/config/cloudinary.js
@@ -0,0 +1,9 @@
+import {v2 as cloudary}  from "cloudinary"
+const connectCloudinary = async () =>{
+    cloudary.config({
+        cloud_name: process.env.CLOUDINARY_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET
+    })
+}
+export default connectCloudinary
--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ import express from "express"
 import cors from "cors";
 import "dotenv/config";
 import connectDb from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
 
 const app = express()
 const port = process.env.PORT || 4000;
 app.use(express.json())
 app.use(cors())
 connectDb()
+connectCloudinary()
 
 app.get( "/" , (req,res) => {
 res.send("API WORING with api")
