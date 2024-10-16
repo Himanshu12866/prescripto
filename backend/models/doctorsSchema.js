f4620446e4a210080303ffa4eec6409217f1c4bb
Himanshu12866 <manshu010m@gmail.com>
2024-10-16 17:44:02 +0530

    "Added doctor schema and model to doctorsSchema.js file"

--- backend/models/doctorsSchema.js
+++ backend/models/doctorsSchema.js
@@ -1,1 +1,19 @@
 import mongoose from "mongoose";
+const doctorSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    image: { type: String, required: true },
+    speciality: { type: String, required: true },
+    degree: { type: String, required: true },
+    experience: { type: String, required: true },
+    about: { type: String, required: true },
+    available: { type: Boolean, required: true },
+    address: { type: Object, required: true },
+    fees: { type: Number, required: true },
+    date: { type: Number, required: true },
+    slot_booked: { type: Object, default: {} },
+}, { minimize: false })
+
+const doctorModal = mongoose.models.doctor || mongoose.model("doctor", doctorSchema)
+export default doctorModal
