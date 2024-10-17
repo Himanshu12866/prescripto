36fa9721a03c7eb21393c8fb30ed18a416e86020
Himanshu12866 <manshu010m@gmail.com>
2024-10-17 18:45:28 +0530

    "Added authAdmin middleware to /add-doctor route"

---
+++ backend/middlewares/authAdmin.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken"
+const authAdmin = async (req, res, next) => {
+    try {
+
+        const { adminToken } = req.headers
+        if (!adminToken) {
+            return res.status(401).json({ msg: "No token, authorization denied", success: false })
+        }
+        const decoded = jwt.verify(adminToken, process.env.SECRET_KEY)
+        if (decoded !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
+            return res.status(401).json({ msg: "No token, authorization denied", success: false })
+
+        }
+        next()
+    } catch (error) {
+
+    }
+}
+export default authAdmin
