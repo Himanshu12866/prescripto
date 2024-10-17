4db91fd5816614dcb858c3dd3b880a2fb451289d
Himanshu12866 <manshu010m@gmail.com>
2024-10-17 16:57:25 +0530

    "Fix typo in import and variable name: 'cloudary' -> 'cloudinary'"

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,6 +1,6 @@
-import {v2 as cloudary}  from "cloudinary"
+import {v2 as cloudinary}  from "cloudinary"
 const connectCloudinary = async () =>{
-    cloudary.config({
+    cloudinary.config({
         cloud_name: process.env.CLOUDINARY_NAME,
         api_key: process.env.CLOUDINARY_API_KEY,
         api_secret: process.env.CLOUDINARY_API_SECRET
