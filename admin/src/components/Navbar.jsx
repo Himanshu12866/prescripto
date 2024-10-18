e567a2bfce0b99c4764924910db6628a20e55efc
Himanshu12866 <manshu010m@gmail.com>
2024-10-18 19:06:13 +0530

    "Added AdminContext, logout functionality, and conditional rendering to Navbar component"

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,10 +1,21 @@
+import { useContext } from "react"
 import { assets } from "../assets/assets"
+import { AdminContext } from "../context/AdminContext"
 const Navbar = () => {
+    const {aToken ,setAToken} = useContext(AdminContext)
+    function LogOut(){
+        aToken && setAToken('')
+        aToken && localStorage.removeItem("AdminToken")
+
+    }
+
   return (
-    <div>
-        <div>
-            <img src={assets.logo}/>
+    <div className="flex items-center justify-between border border-gray-300 sm:px-10 py-3 px-4 w-full">
+        <div className="flex items-center text-xs gap-3">
+            <img className="w-36 sm:w-40 cursor-pointer" src={assets.logo}/>
+            <p className="font-medium text-sm border rounded-full bg-gray-100 text-black  shadow-sm px-4 py-1">{aToken ? "Admin" : "Doctor"}</p>
         </div>
+        <button onClick={LogOut} className="bg-gray-100 text-black font-medium px-4 py-1 shadow-sm rounded-full">Log Out</button>
     </div>
   )
 }
