dd977e022c61e2e635c404354ab796916d650880
Himanshu12866 <manshu010m@gmail.com>
2024-10-18 14:41:03 +0530

    "Updated authAdmin middleware: added whitespace, changed SECRET_KEY to JWT_SECRET, and added semicolons."

--- backend/middlewares/authAdmin.js
+++ backend/middlewares/authAdmin.js
@@ -2,16 +2,21 @@ import jwt from "jsonwebtoken"
 const authAdmin = async (req, res, next) => {
     try {
 
-        const { adminToken } = req.headers
+        const  {adminToken}  = req.headers
+
         if (!adminToken) {
+
             return res.status(401).json({ msg: "No token, authorization denied", success: false })
         }
-        const decoded = jwt.verify(adminToken, process.env.SECRET_KEY)
+
+        const decoded = jwt.verify(adminToken, process.env.JWT_SECRET)
+       
         if (decoded !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
+        
             return res.status(401).json({ msg: "No token, authorization denied", success: false })
 
         }
-        next()
+        next();
     } catch (error) {
 
     }
