84b2a83df7f71ac9e0b5b3282b8ca944c6959ff3
Himanshu12866 <manshu010m@gmail.com>
2024-10-19 19:03:34 +0530

    "Added AdminContextProvider with getAllDoctors function and updated DoctorList component to use it"

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -2,12 +2,31 @@
 // /* eslint-disable no-unused-vars */
 import { createContext, useState } from "react";
 export const AdminContext = createContext()
+import axios from "axios"
+import {toast} from "react-toastify"
+
 
 const AdminContextProvider = (props) => {
+    const [doctors, setDoctors] = useState([])
     const [aToken, setAToken] = useState(localStorage.getItem('AdminToken') ? localStorage.getItem("AdminToken") : "")
     const backendURL = import.meta.env.VITE_BACKEND_URL
+  
+    const getAllDoctors = async () => {
+        try {
+            const {data} = await axios.post(backendURL + '/api/admin/all-doctors' , {} , {headers:{aToken}})
+            if(data.success) {
+            setDoctors(data.doctors)
+            console.log(data.doctors)
+            }
+            else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error)
+        }
+    }
     const value = {
-        aToken, setAToken, backendURL
+        aToken, setAToken, backendURL,doctors,getAllDoctors
     }
     return (
         <AdminContext.Provider value={value}>
--- admin/src/pages/Admin/DoctorList.jsx
+++ admin/src/pages/Admin/DoctorList.jsx
@@ -1,8 +1,23 @@
 // import React from 'react'
 
+import { useContext, useEffect } from "react"
+import { AdminContext } from "../../context/AdminContext"
+
 const DoctorList = () => {
+
+  const {doctors , aToken , getAllDoctors} = useContext(AdminContext)
+  useEffect(() => {
+    if(aToken){
+      getAllDoctors()
+      console.log(doctors)
+    }
+  },[aToken])
   return (
-    <div>DoctorList</div>
+
+    <div>
+
+
+    </div>
   )
 }
 
