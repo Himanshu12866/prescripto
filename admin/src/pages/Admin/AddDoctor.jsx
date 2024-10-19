31217a435bdcf6f5325fbc0d1afc42b7138d3411
Himanshu12866 <manshu010m@gmail.com>
2024-10-19 16:31:21 +0530

    "Updated AddDoctor component with layout and styling changes"

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -6,10 +6,10 @@ import { assets } from "../../assets/assets"
 
 const AddDoctor = () => {
     return (
-        <form className=" bg-[#b0a8a81c] w-full">
+        <form className=" bg-[#b0a8a81c] w-full flex justify-center">
 
+            <div className="sm:w-[90%] w-full ml-3 mt-2 overflow-y-scroll h-[80vh]">
             <p className="font-medium w-full text-start text-xl px-12  py-2 ">Add Doctor</p>
-            <div className="w-52 ml-3 mt-2">
                 <div className="flex items-center gap-2 w-[200px]">
                     <label htmlFor="doc_img">
                         <img className="w-[80px] h-[80px] rounded-full cursor-pointer" src={assets.image_upload} />
@@ -17,23 +17,23 @@ const AddDoctor = () => {
                     <input required type="file" id="doc_img" hidden />
                     <p className="text-xs">Upload Doctor <br></br> image</p>
                 </div>
-                <div>
-                    <div>
-                        <div>
+                <div className="flex sm:flex-row gap-10 flex-col w-full">
+                    <div className=" sm:p-5" >
+                        <div >
                             <p>Doctor Name :</p>
-                            <input required type="text" placeholder="Full Name" />
+                            <input required className="p-2 my-1 min-w-64 sm:w-80 border rounded-sm" type="text" placeholder="Full Name" />
                         </div>
                         <div>
                             <p>Doctor Email :</p>
-                            <input required type="email" placeholder="Email" />
+                            <input className="p-2 my-1 min-w-64 sm:w-80 border rounded-sm" required type="email" placeholder="Email" />
                         </div>
                         <div>
                             <p>Doctor Password :</p>
-                            <input required type="password" placeholder="Password" />
+                            <input className="p-2 my-1 min-w-64 sm:w-80 border rounded-sm" required type="password" placeholder="Password" />
                         </div>
                         <div>
                             <p>Experience :</p>
-                            <select>
+                            <select className="p-2 my-1 min-w-64 sm:w-80 border rounded-sm">
                                 <option value="0 Years">0 Years</option>
                                 <option value="1 Years">1 Years</option>
                                 <option value="2 Years">2 Years</option>
@@ -50,13 +50,13 @@ const AddDoctor = () => {
                         </div>
                         <div>
                             <p>Fees</p>
-                            <input required type="number" placeholder="Fees" />
+                            <input className="p-2 my-1 min-w-64 sm:w-80 border rounded-sm" required type="number" placeholder="Fees" />
                         </div>
                     </div>
-                    <div>
+                    <div className="mt-5">
                         <div>
                             <p>Speciality :</p>
-                            <select name="" id="" required>
+                            <select className="p-2 my-1 min-w-64 sm:w-80 border rounded-sm" name="" id="" required>
                                 <option value="General physician">General physician</option>
                                 <option value="Gynecologist">Gynecologist</option>
                                 <option value="Dermatologist">Dermatologist</option>
@@ -67,14 +67,18 @@ const AddDoctor = () => {
                         </div>
                         <div>
                             <p>Degree :</p>
-                            <input type="text" placeholder="Degree" required/>
+                            <input className="p-2 my-1 min-w-64 sm:w-80 border rounded-sm" type="text" placeholder="Degree" required/>
                         </div>
                         <div>
                             <p>Address :</p>
-                            <input type="text" placeholder="Line 1"  required/>
-                            <input type="text" placeholder="Line 1" required />
+                            <input className="p-2 my-1 min-w-64 sm:w-80 border rounded-sm" type="text" placeholder="Line 1"  required/>
+                            <input className="p-2 my-1 min-w-64 sm:w-80 border rounded-sm" type="text" placeholder="Line 1" required />
                         </div>
                     </div>
+                </div>
+                <div className="sm:w-[80%] w-full ml-3 mt-2">
+                    <p>About</p>
+                    <textarea className="w-full"  placeholder="Some Doctor's Summary" required/>
                 </div>
 
             </div>
