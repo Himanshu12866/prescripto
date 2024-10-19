ed774d9a4d19044fd5923813378398f0839080d9
Himanshu12866 <manshu010m@gmail.com>
2024-10-19 16:51:07 +0530

    "Added state variables and updated image upload functionality in AddDoctor component"

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -1,21 +1,38 @@
 // import React from 'react'
 
+import { useState } from "react"
 import { assets } from "../../assets/assets"
 
 // import { assets } from "../../assets"
 
 const AddDoctor = () => {
+
+    const [image, setImage] = useState(false);
+    const [name, setName] = useState("")
+    const [specialization, setSpecialization] = useState("General physician");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [address1, setAddress1] = useState("");
+    const [address2, setAddress2] = useState("");
+    const [about, setAbout] = useState("");
+    const [fees, setfees] = useState("");
+    const [experience, setExperience] = useState("1 Year");
+    const [degree, setDegree] = useState("");
+
+
     return (
         <form className=" bg-[#b0a8a81c] w-full flex justify-center h-auto">
 
             <div className="sm:w-[90%] w-full ml-3 mt-2  h-auto overflow-y-auto">
-            <p className="font-medium w-full text-start text-xl px-12  py-2 ">Add Doctor</p>
+                <p className="font-medium w-full text-start text-xl px-12  py-2 ">Add Doctor</p>
                 <div className="flex items-center gap-2 w-[200px]">
                     <label htmlFor="doc_img">
-                        <img className="w-[80px] h-[80px] rounded-full cursor-pointer" src={assets.image_upload} />
+                        <img className="w-[80px] h-[80px] rounded-full cursor-pointer" src={image ? URL.createObjectURL(image) : assets.image_upload} />
                     </label>
-                    <input required type="file" id="doc_img" hidden />
-                    <p className="text-xs">Upload Doctor <br></br> image</p>
+                    <input required type="file" onChange={(e) => setImage(e.target.files[0])} id="doc_img" hidden />
+                    {
+                        image ? <p>Uploaded</p> : <p className="text-xs">Upload Doctor <br></br> image</p>
+                    }
                 </div>
                 <div className="flex sm:flex-row gap-10 flex-col w-full">
                     <div className=" sm:p-5" >
@@ -67,21 +84,21 @@ const AddDoctor = () => {
                         </div>
                         <div>
                             <p>Degree :</p>
-                            <input className="p-2 my-1 min-w-64 sm:w-80 border rounded-sm" type="text" placeholder="Degree" required/>
+                            <input className="p-2 my-1 min-w-64 sm:w-80 border rounded-sm" type="text" placeholder="Degree" required />
                         </div>
                         <div>
                             <p>Address :</p>
-                            <input className="p-2 my-1 min-w-64 sm:w-80 border rounded-sm" type="text" placeholder="Line 1"  required/>
+                            <input className="p-2 my-1 min-w-64 sm:w-80 border rounded-sm" type="text" placeholder="Line 1" required />
                             <input className="p-2 my-1 min-w-64 sm:w-80 border rounded-sm" type="text" placeholder="Line 1" required />
                         </div>
                     </div>
                 </div>
                 <div className="sm:w-[70%] w-[80%]  ml-3 mt-2">
                     <p>About</p>
-                    <textarea className="w-full p-2" rows="5"  placeholder="Some Doctor's Summary" required/>
+                    <textarea className="w-full p-2" rows="5" placeholder="Some Doctor's Summary" required />
                 </div>
 
-            <button className="w-52 rounded-full my-10 bg-[blue] p-2 text-white">Click</button>
+                <button className="w-52 rounded-full my-10 bg-[blue] p-2 text-white">Click</button>
             </div>
 
 
