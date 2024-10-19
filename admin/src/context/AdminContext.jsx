2c286d16c8abef91ce63db24d39262917b6d4c54
Himanshu12866 <manshu010m@gmail.com>
2024-10-19 18:40:57 +0530

    "Uncommented eslint-disable no-unused-vars directive in AdminContext.jsx"

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-/* eslint-disable no-unused-vars */
+// /* eslint-disable no-unused-vars */
 import { createContext, useState } from "react";
 export const AdminContext = createContext()
 
