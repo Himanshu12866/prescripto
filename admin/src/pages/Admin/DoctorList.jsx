0511bbbdf9e70d49ab2841dd03cbce4b6fc75b1d
Himanshu12866 <manshu010m@gmail.com>
2024-10-22 15:53:28 +0530

    "Added useState to DoctorList component, updated useEffect and JSX structure"

--- admin/src/pages/Admin/DoctorList.jsx
+++ admin/src/pages/Admin/DoctorList.jsx
@@ -1,21 +1,36 @@
 // import React from 'react'
 
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
 import { AdminContext } from "../../context/AdminContext"
 
 const DoctorList = () => {
+  const [doctor , setDoctors] = useState([{}])
 
-  const {doctors , aToken , getAllDoctors} = useContext(AdminContext)
+  const { doctors, aToken, getAllDoctors } = useContext(AdminContext)
   useEffect(() => {
-    if(aToken){
+    if (aToken) {
       getAllDoctors()
+      // setDoctors(doctors)
+      setDoctors(doctors)
       console.log(doctors)
+
+
     }
-  },[aToken])
+  }, [aToken])
   return (
 
     <div>
-
+      <h1>All Doctors</h1>
+      <div>
+        {
+          doctor.map((item, index) => {
+            <div key={index}>
+              <img src={item.image} />
+              <h1>{item.name}</h1>
+            </div>
+          })
+        }
+      </div>
 
     </div>
   )
