30649568f06264886c0ce3095ab5144e13f6cb11
Himanshu12866 <manshu010m@gmail.com>
2024-10-22 17:08:21 +0530

    "Uncommented and reformatted code in AdminContext.jsx and doctorController.js"

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -5,10 +5,12 @@ const checkAvailablity = async (req, res) => {
     try {
         const { docId } = req.body;
         const docData = await doctorModal.findById(docId)
-
         await doctorModal.findByIdAndUpdate(docId , {available : !docData.available})
         res.json({message : "Doctor's Availability Changed" , success:true})
-    } catch (error) {
+
+    } 
+    catch (error) {
+        
         console.error(error);
         res.json({ success: false, message: error.message })
 
