6abff8a12de030823f24118e249d3073502f73b1
Himanshu12866 <manshu010m@gmail.com>
2024-10-22 17:49:12 +0530

    "Added export statement to doctorRoute.js and imported doctorRouter in server.js, then mounted it at /api/doctor"

--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -1,7 +1,6 @@
-
-
 import express from "express"
 import { doctorList } from "../controllers/doctorController.js"
 
 const doctorRouter = express.Router()
 doctorRouter.get('/list' , doctorList)
+export default doctorRouter
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import connectDb from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
 import adminRouter from "./routes/adminRoute.js";
+import doctorRouter from "./routes/doctorRoute.js";
 
 const app = express()
 const port = process.env.PORT || 4000;
@@ -14,10 +15,11 @@ app.use(express.json())
 app.use(cors())
 
 
-app.use("/api/admin" , adminRouter)
+app.use("/api/admin", adminRouter)
+app.use("/api/doctor", doctorRouter)
 
-app.get( "/" , (req,res) => {
-    
-res.send("API WORING with api")
+app.get("/", (req, res) => {
+
+    res.send("API WORING with api")
 })
-app.listen(port , () => console.log("Server Sarted at port number" , port))
+app.listen(port, () => console.log("Server Sarted at port number", port))
