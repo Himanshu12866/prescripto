1846b617af2a69c6bf47fb7cf549de2a729abffb
Himanshu12866 <manshu010m@gmail.com>
2024-10-22 16:47:53 +0530

    "Added doctor availability toggle functionality to admin route"

---
+++ backend/controllers/doctorController.js
@@ -0,0 +1,18 @@
+import doctorModal from "../models/doctorModal";
+
+
+const checkAvailablity = async (req, res) => {
+    try {
+        const { docId } = req.body;
+        const docData = await doctorModal.findById(docId)
+
+        await doctorModal.findByIdAndUpdate(docId , {available : !docData.available})
+        res.json({message : "Doctor's Availability Changed" , success:true})
+    } catch (error) {
+        console.error(error);
+        res.json({ success: false, message: error.message })
+
+    }
+
+}
+export { checkAvailablity }
--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import { addDoctor, allDoctors, loginAdmin } from "../controllers/adminController.js";
 import upload from "../middlewares/multer.js";
 import authAdmin from "../middlewares/authAdmin.js";
+import { checkAvailablity } from "../controllers/doctorController.js";
 
 
 const adminRouter = express.Router()
@@ -9,5 +10,6 @@ const adminRouter = express.Router()
 adminRouter.post("/add-doctor", authAdmin, upload.single("image"), addDoctor)
 adminRouter.post("/login", loginAdmin)
 adminRouter.post("/all-doctors", authAdmin, allDoctors)
+adminRouter.post("/change-availablity", authAdmin, checkAvailablity)
 
 export default adminRouter
