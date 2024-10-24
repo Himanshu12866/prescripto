9fcbcc926e43fdfd0fa1cfeb5ad7ff3bf8dfbe24
Himanshu12866 <manshu010m@gmail.com>
2024-10-24 18:11:04 +0530

    "Added useContext and useState hooks to MyAppointment component, imported AppContext, and declared backendURL, token, and appData variables."

--- frontend/src/pages/MyAppointment.jsx
+++ frontend/src/pages/MyAppointment.jsx
@@ -1,7 +1,12 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { AppContext } from '../context/AppContext'
 
 const MyAppointment = () => {
+
+  const {backendURL , token} = useContext(AppContext)
+  const [ appData , setAppData] = useState([])
+  
   return (
     <div>
       <h1>booked</h1>
