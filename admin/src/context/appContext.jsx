b873dcd2e26cfde92239646e3f5a55950a16eb11
Himanshu12866 <manshu010m@gmail.com>
2024-10-26 17:18:39 +0530

    "Updated title tags in admin and frontend index.html files, added SlotFormat function and month array in appContext, and removed modal code in AllAppointment component."

--- admin/src/context/appContext.jsx
+++ admin/src/context/appContext.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import { createContext } from "react";
 
@@ -5,8 +6,20 @@ export const AppContext = createContext()
 
 
 const AppContextProvider = (props) => {
+    // const Calage = (dob) => {
+    //     const today = new Date();
+    //     const bdate = new Date(dob);
+    //     let age = today.getFullYear() - bdate.getFullYear();
+    //     return age;
+
+    // }
+    const month = [" ", "Jan", "Fer", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    const SlotFormat = (slotDate) => {
+      const formattedTime = slotDate.split("-");
+      return formattedTime[0] + " " + month[Number(formattedTime[1])] + " " + formattedTime[2]
+    }
     const values = {
-     
+     SlotFormat
     }
     return (
         <AppContext.Provider value={values}>
--- admin/src/pages/Admin/AllAppointment.jsx
+++ admin/src/pages/Admin/AllAppointment.jsx
@@ -2,17 +2,15 @@ import { useContext } from "react";
 import { useEffect } from "react";
 import { AdminContext } from "../../context/AdminContext";
 import Button from '@mui/material/Button';
-// import { Modal, Box, Typography } from "@mui/material";
+// import { AppContext } from "../../context/appContext";
+
 
 const AllAppointment = () => {
   const { appointments, aToken, getAllAppointment } = useContext(AdminContext);
+  // const { SlotFormat } = useContext(AppContext)
 
-  // State to manage modal open/close
-  // const [open, setOpen] = useState(false);
 
-  // // Handlers to open and close modal
-  // const handleOpen = () => setOpen(true);
-  // const handleClose = () => setOpen(false);
+
 
   useEffect(() => {
     if (aToken) {
@@ -23,18 +21,6 @@ const AllAppointment = () => {
     }
   }, [aToken]);
 
-  // Define the modal box style
-  // const style = {
-  //   position: 'absolute',
-  //   top: '50%',
-  //   left: '50%',
-  //   transform: 'translate(-50%, -50%)',
-  //   width: 400,
-  //   bgcolor: 'background.paper',
-  //   boxShadow: 24,
-  //   p: 4,
-  //   borderRadius: 2,
-  // };
 
   return (
     <div className="w-full p-1">
@@ -52,12 +38,12 @@ const AllAppointment = () => {
       </div>
       {appointments.map((item, index) => (
         <div key={index}>
-          <div className="border py-2 px-2 mt-3 hidden sm:grid grid-cols-[0.5fr_3fr_1fr_4fr_3fr_1fr_2fr] grid-flow-col">
+          <div className="border py-2 px-2 mt-3 sm:grid grid-cols-[0.5fr_3fr_1fr_4fr_3fr_1fr_2fr] grid-flow-col">
             <p>{index + 1}</p>
             <p className="text-center font-medium">{item.userData?.name}</p>
             <p className="text-[15px]">{item.userData?.email}</p>
             <p className="text-center">
-              <span>{item.slotDate || "Na"}</span> &nbsp; <span>{item.slotTime || "Na"}</span>
+              <span>{(item.slotDate) || "Na"}</span> &nbsp; <span>{item.slotTime || "Na"}</span>
             </p>
             <div className="flex flex-row gap-4" >
               <img style={{ width: "40px", height: "35px", borderRadius: "50%" }} src={item.docData?.image} />
@@ -70,27 +56,9 @@ const AllAppointment = () => {
               </Button>
             </p>
           </div>
-         
+
         </div>
       ))}
-      {/* <Button onClick={handleOpen} variant="contained">Open Modal</Button> */}
-
-      {/* <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-          >
-            <Box sx={style}>
-              <Typography id="modal-modal-title" variant="h6" component="h2">
-                <p>{item.docData?.name}</p>
-              </Typography>
-              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            <img src={item.docData?.image} />
-              </Typography>
-            </Box>
-          </Modal> */}
-
     </div>
   );
 }
