e330542826fb2f25cbb554f773d74e76c79c709e
Himanshu12866 <manshu010m@gmail.com>
2024-10-30 15:39:41 +0530

    "Updated DocProfile component to enable editing and saving of doctor profile information"

--- admin/src/pages/Doctor/DocProfile.jsx
+++ admin/src/pages/Doctor/DocProfile.jsx
@@ -1,20 +1,46 @@
 import { useContext, useEffect, useState } from "react"
 import { DoctorContext } from "../../context/doctorContext"
 import { Button, TextField, FormGroup, FormControlLabel, Checkbox, FormControl, Select, MenuItem, InputLabel } from "@mui/material"
+import axios from "axios"
+import { toast } from "react-toastify"
 
 
 const DocProfile = () => {
-    const { docToken, profile, getProfile } = useContext(DoctorContext)
+    const { docToken, profile, setProfile, getProfile, backendURL } = useContext(DoctorContext)
     const [edit, setEdit] = useState(false)
-    const [age, setAge] = useState('');
 
-    const handleChange = (event) => {
-        setAge(event.target.value);
-        console.log(age)
-    };
+    const updataDocProfile = async () => {
+        try {
+            const updatedprofileData = {
+                name: setProfile.name,
+
+                fees: setProfile.fees,
+                experience: setProfile.experience,
+                about: setProfile.about,
+                degree: setProfile.degree,
+                available: setProfile.available,
+                address: setProfile.address
+
+            }
+            const { data } = await axios.post(backendURL + '/api/doctor/updateprofile', { updatedprofileData }, { headers: { docToken } })
+            if (data.success) {
+                setEdit(false)
+                toast.success("Profile Updated Successfully")
+            }
+            else {
+                toast.error("Failed to Update Profile")
+            }
+        } catch (error) {
+
+            console.log(error)
+        }
+    }
+
+
     useEffect(() => {
         getProfile()
         console.log(profile)
+        console.log(profile.available)
 
     }, [docToken])
     return (
@@ -31,21 +57,21 @@ const DocProfile = () => {
                             <p className="w-64 border-b-2">Name :</p>
 
                             {
-                                edit ? <TextField id="outlined-basic" label="Enter Name" className="w-72" variant="outlined" margin="none" /> : <p className="p-1 w-64 border-b-2" type="text">{profile.name}</p>
+                                edit ? <TextField id="outlined-basic" label="Enter Name" className="w-72" value={profile.name} onChange={(e) => { setProfile(prev => ({ ...prev, name: e.target.value })) }} variant="outlined" margin="none" /> : <p className="p-1 w-64 border-b-2" type="text">{profile.name}</p>
                             }
                         </div>
                         <div className="flex flex-row justify-between py-2 w-full">
                             <h1 className="w-52 border-b-2">Email :</h1>
 
                             {
-                                edit ? <TextField id="outlined-basic" value={profile.email} className="w-72" variant="outlined" margin="none" /> : <p className="p-1 w-64 border-b-2" type="text">{profile.email}</p>
+                                edit ? <TextField id="outlined-basic" label="Email" onKeyDown={() => { alert("Sorry Email Cannot be change") }} value={profile.email} className="w-72" variant="outlined" margin="none" /> : <p className="p-1 w-64 border-b-2" type="text">{profile.email}</p>
                             }
                         </div>
                         <div className="flex flex-row  justify-between py-2 w-full">
                             <h1 className="w-52 border-b-2">Fees :</h1>
 
                             {
-                                edit ? <TextField id="outlined-basic" label="Enter Fees" className="w-72" variant="outlined" margin="none" /> : <p className="p-1 w-64 border-b-2" type="text">{profile.fees}</p>
+                                edit ? <TextField id="outlined-basic" label="Enter Fees" value={profile.fees} onChange={(e) => { setProfile(prev => ({ ...prev, fees: e.target.value })) }} className="w-72" variant="outlined" margin="none" /> : <p className="p-1 w-64 border-b-2" type="text">{profile.fees}</p>
                             }
                         </div>
                         <div className="flex flex-row justify-between py-2 w-full">
@@ -53,8 +79,10 @@ const DocProfile = () => {
 
                             {
                                 edit ? <FormGroup className="w-72">
-                                    <FormControlLabel control={<Checkbox defaultChecked />} label="Available" />
-                                </FormGroup> : <p className="p-1 w-64 border-b-2" type="text">{profile.name}</p>
+                                    <FormControlLabel control={<Checkbox checked={profile.available ? true : false} onChange={() => { setProfile(prev => ({ ...prev, available: !prev.available })) }} />} label="Available" />
+                                </FormGroup> : <FormGroup className="w-72">
+                                    <FormControlLabel className="px-8" control={<Checkbox checked={profile.available?true:false} />}  label="Available" />
+                                </FormGroup>
                             }
                         </div>
                         <div className="flex flex-row justify-between py-2 w-full">
@@ -66,8 +94,8 @@ const DocProfile = () => {
                                     <Select
                                         labelId="demo-simple-select-standard-label"
                                         id="demo-simple-select-standard"
-                                        value={age}
-                                        onChange={handleChange}
+                                        value={profile.experience}
+                                        onChange={(e) => { setProfile(prev => ({ ...prev, experience: e.target.value })) }}
                                         label="Experience"
                                     >
                                         {/* <MenuItem value="">
@@ -92,7 +120,7 @@ const DocProfile = () => {
                             <h1 className="w-52 border-b-2">Degree :</h1>
 
                             {
-                                edit ? <TextField id="outlined-basic" label="Enter Degree" className="w-72" variant="outlined" margin="none" /> : <p className="p-1 w-64 border-b-2" type="text">{profile.degree}</p>
+                                edit ? <TextField id="outlined-basic" label="Enter Degree" value={profile.degree} onChange={(e) => { setProfile(prev => ({ ...prev, degree: e.target.value })) }} className="w-72" variant="outlined" margin="none" /> : <p className="p-1 w-64 border-b-2" type="text">{profile.degree}</p>
                             }
                         </div>
                         <div className="flex flex-row justify-between py-2 w-full">
@@ -100,9 +128,9 @@ const DocProfile = () => {
 
 
                             {
-                                edit ? <div className="flex flex-col gap-1">
-                                    <TextField id="outlined-basic" label="Enter Degree" className="w-72" variant="outlined" margin="none" />
-                                    <TextField id="outlined-basic" label="Enter Degree" className="w-72" variant="outlined" margin="none" />
+                                edit ? <div className="flex flex-col gap-2">
+                                    <TextField id="outlined-basic" label="Line 1" value={profile.address.line1} onChange={(e) => { setProfile(prev => ({ ...prev, address: { ...prev, line1: e.target.value } })) }} className="w-72" variant="outlined" margin="none" />
+                                    <TextField id="outlined-basic" label="Line 2" value={profile.address.line2} onChange={(e) => { setProfile(prev => ({ ...prev, address: { ...prev, line2: e.target.value } })) }} className="w-72" variant="outlined" margin="none" />
                                 </div> : <div className="w-64">
                                     {/* <p className=" text-left">{profile.address.line1}</p>
                                     <p className=" text-left">{profile.address.line2}</p> */}
@@ -114,7 +142,7 @@ const DocProfile = () => {
                         <div className="flex flex-row py-2 justify-between w-full">
                             <h1 className="w-64 border-b-2">About :</h1>
 
-                            {edit ? <TextField className="p-1  w-[600px] mx-5 border-b-2" label="About" /> : <p className="p-1  w-[600px] border-b-2" type="text">{profile.about}</p>}
+                            {edit ? <TextField className="p-1  w-[600px] mx-5 border-b-2" label="About" multiline rows="4" value={profile.about} onChange={(e) => { setProfile(prev => ({ ...prev, about: e.target.value })) }} /> : <p className="p-1  w-[600px] border-b-2" type="text">{profile.about}</p>}
                         </div>
 
 
@@ -122,12 +150,12 @@ const DocProfile = () => {
                 </div>
             </div>
             <div>
-                {edit ? <div className="flex justify-center gap-2 w-[50%] p-3"> 
-                    <Button style={{padding:"10px"}} variant="contained" color="success" className="w-52">Save</Button>
-                    <Button style={{padding:"10px"}} className="w-52 " color="error" variant="contained" onClick={() => {
+                {edit ? <div className="flex justify-center gap-2 w-[50%] p-3">
+                    <Button style={{ padding: "10px" }} variant="contained" color="success" onClick={updataDocProfile} className="w-52">Save</Button>
+                    <Button style={{ padding: "10px" }} className="w-52 " color="error" variant="contained" onClick={() => {
                         setEdit(false);
                     }}>Cancel</Button>
-                </div> : <Button  className="w-80" style={{padding:"10px"}} variant="contained" onClick={() => { setEdit(true); scrollTo(0,500) }}>Edit Details</Button>}
+                </div> : <Button className="w-80" style={{ padding: "10px" }} variant="contained" onClick={() => { setEdit(true); scrollTo(0, 500) }}>Edit Details</Button>}
             </div>
         </div>
 
