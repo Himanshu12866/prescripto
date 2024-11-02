1fadc79716104369a7b8ce02827ac84a5f5e4e51
Himanshu12866 <manshu010m@gmail.com>
2024-11-02 19:19:06 +0530

    "Updated class names for responsive padding in AllAppointment component"

--- admin/src/pages/Admin/AllAppointment.jsx
+++ admin/src/pages/Admin/AllAppointment.jsx
@@ -20,7 +20,7 @@ const AllAppointment = () => {
 
   return (
     <div className="w-full p-1">
-      <p className="px-5 py-3 font-medium w-full bg-black text-center text-white">All Appointment</p>
+      <p className="px-5 sm:px-2 md:px-3 lg:px-3 py-3 font-medium w-full bg-black text-center text-white">All Appointment</p>
       <div className="w-full">
         <div className="border py-2 px-2 mt-3 bg-[#042613b8] text-[#decddb] font-bold hidden sm:grid grid-cols-[0.5fr_3fr_1fr_4fr_3fr_1fr_2fr] grid-flow-col">
           <p>Sr.No</p>
