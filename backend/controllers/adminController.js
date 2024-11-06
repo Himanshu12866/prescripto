428bb35480fddefb80ba54e0f4a10dca986a5059
Himanshu12866 <manshu010m@gmail.com>
2024-11-06 17:33:46 +0530

    "Updated import of bcrypt from 'bcryptjs' to 'bcrypt'"

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,5 +1,5 @@
 import validator from "validator";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 import { v2 as cloudinary } from "cloudinary";
 import doctorModal from "../models/doctorModal.js";
 import jwt from "jsonwebtoken"
