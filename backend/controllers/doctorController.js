355b201f477b226603388a73a77665dbe35aaca6
Himanshu12866 <manshu010m@gmail.com>
2024-11-06 16:58:12 +0530

    "Updated bcrypt import from 'bcrypt' to 'bcryptjs' in doctorController.js and userController.js"

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -1,5 +1,5 @@
 import doctorModal from "../models/doctorModal.js";
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 import jwt from "jsonwebtoken"
 import appointmentModel from "../models/appointment.js";
 
